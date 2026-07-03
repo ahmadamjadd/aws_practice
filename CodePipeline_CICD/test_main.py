314bf5806ede9adb905548cd061c79686dc72e52
ahmadamjadd <muhammadahmadamjad0@gmail.com>
2026-07-03 14:29:29 +0500

    first commit

---
+++ CodePipeline_CICD/test_main.py
@@ -0,0 +1,12 @@
+import pytest
+from main import app
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_hello(client):
+    rv = client.get('/')
+    assert b'Hello from GitHub to AWS Pipeline' in rv.data
